32e8124d8413ee3b824f1ff1ef9ccd2bfc1be2b6
Sandwich2080 <androidsandwich@163.com>
2020-02-10 15:28:10 +0800

    [Modified] adjust the border and border radius params of GridView.

--- lib/common/layouts/gridview_example.dart
+++ lib/common/layouts/gridview_example.dart
@@ -9,6 +9,10 @@ class GridViewExampleState extends State<GridViewExample> {
   List<Widget> buildItems() {
     return List<Container>.generate(20, (int index) {
       return Container(
+        decoration: BoxDecoration(
+            color: Colors.amberAccent,
+            border: Border.all(width: 0.1, color: Colors.white),
+            borderRadius: BorderRadius.all(Radius.circular(0.1))),
         child: Image.asset("assets/images/logo.png"),
       );
     });
