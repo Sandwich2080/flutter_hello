4cfb152bd356ca3990397e04d4d466b8b2927cd5
Sandwich2080 <androidsandwich@163.com>
2020-02-10 15:17:04 +0800

    [Modified] Add an example for 'Card'.

--- lib/common/layouts/card_example.dart
+++ lib/common/layouts/card_example.dart
@@ -8,7 +8,46 @@ class CardExample extends StatefulWidget {
 class CardExampleState extends State<CardExample> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    var card = SizedBox(
+      height: 210,
+      child: Card(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: new Text('1625 Main Street',
+                  style: new TextStyle(fontWeight: FontWeight.w500)),
+              subtitle: new Text('My City, CA 99984'),
+              leading: new Icon(
+                Icons.restaurant_menu,
+                color: Colors.blue[500],
+              ),
+            ),
+            Divider(),
+            ListTile(
+              title: new Text('(408) 555-1212',
+                  style: new TextStyle(fontWeight: FontWeight.w500)),
+              leading: new Icon(
+                Icons.contact_phone,
+                color: Colors.blue[500],
+              ),
+            ),
+            ListTile(
+              title: new Text('costa@example.com'),
+              leading: new Icon(
+                Icons.contact_mail,
+                color: Colors.blue[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Card Example"),
+      ),
+      body: card,
+    );
   }
 }
