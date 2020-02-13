9aea2fd09075d04b8e8b86d44b85fc031bf4a9c6
Sandwich2080 <androidsandwich@163.com>
2020-02-13 10:27:13 +0800

    [Modified] example for 'reading external files'

--- lib/common/io/sd_card_files.dart
+++ lib/common/io/sd_card_files.dart
@@ -17,15 +17,36 @@ class SDCardFilesState extends State<SDCardFiles> {
   void initState() {
     super.initState();
 
+    // Fetch external directories on SD Card.
+    getExternalStorageDirectories(type: StorageDirectory.dcim).then((List<Directory> dirs){
+      for(int i=0;i<dirs.length;i++){
+        print("dcim:${dirs[i].path}");
+      }
+    });
+    getExternalStorageDirectories(type: StorageDirectory.movies).then((List<Directory> dirs){
+      for(int i=0;i<dirs.length;i++){
+        print("dcim:${dirs[i].path}");
+      }
+    });
+    getExternalStorageDirectories(type: StorageDirectory.documents).then((List<Directory> dirs){
+      for(int i=0;i<dirs.length;i++){
+        print("dcim:${dirs[i].path}");
+      }
+    });
+    getExternalStorageDirectories(type: StorageDirectory.downloads).then((List<Directory> dirs){
+      for(int i=0;i<dirs.length;i++){
+        print("dcim:${dirs[i].path}");
+      }
+    });
+
     getExternalStorageDirectory().then((d) {
       setState(() {
         sdDir = d.path;
         files = d.listSync();
-        
-        for(int i=0;i<files.length;i++){
+
+        for (int i = 0; i < files.length; i++) {
           print("File: ${files[i].path}");
         }
-        
       });
     });
   }
@@ -40,6 +61,7 @@ class SDCardFilesState extends State<SDCardFiles> {
         itemCount: files == null ? 0 : files.length,
         itemBuilder: (BuildContext context, int index) =>
             buildListItem(files[index]),
+        
       ),
     );
   }
@@ -48,7 +70,39 @@ class SDCardFilesState extends State<SDCardFiles> {
     var isFile = FileSystemEntity.isFileSync(file.path);
 
     var fileStat = file.statSync();
-    return Card(
+
+
+    return GestureDetector(
+      child: Card(
+        margin: EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Image.asset(isFile
+                ? "assets/images/img_file.png"
+                : "assets/images/img_folder.png"),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "${file.path.substring(file.parent.path.length + 1)}",
+                  style: TextStyle(color: Colors.black),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("${fileStat.modified}"),
+              ],
+            ),
+          ],
+        ),
+      ),
+      onTap: (){onItemClick(file);},
+    );
+
+
+    /*return Card(
       margin: EdgeInsets.all(10.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -58,16 +112,25 @@ class SDCardFilesState extends State<SDCardFiles> {
               : "assets/images/img_folder.png"),
           Column(
             mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text(
-                "${file.path.substring(file.path.length)}",
+                "${file.path.substring(file.parent.path.length + 1)}",
                 style: TextStyle(color: Colors.black),
+              ),
+              SizedBox(
+                height: 10,
               ),
               Text("${fileStat.modified}"),
             ],
           ),
         ],
       ),
-    );
+    );*/
   }
+
+  void onItemClick(FileSystemEntity file){
+    print("${file.path} is clicked");
+  }
+
 }
