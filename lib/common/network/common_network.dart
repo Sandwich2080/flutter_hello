d999ebbc4307ce97735f64a43b7a4abb9bd20312
Sandwich2080 <androidsandwich@163.com>
2021-01-29 16:02:23 +0800

    [modified] change the log printing  Api

--- lib/common/network/common_network.dart
+++ lib/common/network/common_network.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:developer' as developer;
 
 class CommonNetwork extends StatefulWidget {
   @override
@@ -131,14 +132,14 @@ class CommonNetworkState extends State<CommonNetwork> {
     socket.write("GET / HTTP1.1\r\nhost:$host");
     socket.close();*/
     var desc1 = "Connect via Socket: $host:$port";
-    print(desc1);
+    _printLog(desc1);
     updateRequestState(desc1);
 
     Socket.connect(host, port, timeout: Duration(seconds: 30)).then(
         (Socket socket) {
       var desc2 =
           "Socket connected successfully.\n\n local address:${socket.address}\n\n local port:${socket.port}\n\n encoding:${socket.encoding.name}";
-      print(desc2);
+      _printLog(desc2);
       updateRequestState("$desc1\n\n$desc2");
       socket.flush();
       socket.close();
@@ -149,27 +150,32 @@ class CommonNetworkState extends State<CommonNetwork> {
 
   void socketConnectError(Object error) {
     var desc = "Socket connect error:$error";
-    print(desc);
+    _printLog(desc);
     updateRequestState(desc);
   }
 
   void onClickDio() async {
     String url = "https://api.github.com";
     var desc1 = "Request via dio...\n Dio().get('$url')\n";
-    print(desc1);
+    _printLog(desc1);
     updateRequestState(desc1);
 
     var dio = Dio();
-    var response = await dio.get(url);
+    var response = await dio.get<String>(url);
     var desc2 = "Response statusCode:${response.statusCode}";
 
-    print(desc2);
+    _printLog(desc2);
     updateRequestState("$desc1\n\n $desc2");
 
     var desc3 = "Response body:${response.data}";
 
-    print(desc3);
+    _printLog(desc3);
     updateRequestState("$desc1\n $desc2\n\n $desc3");
+  }
+
+  void _printLog(String desc2) {
+    //print(desc2);
+    developer.log(desc2);
   }
 
   void onClickHttp() async {
@@ -177,28 +183,28 @@ class CommonNetworkState extends State<CommonNetwork> {
     //1. directly using the http api
     String url = "https://api.github.com";
     var desc1 = "Request via http directly...\n http.get('$url')";
-    print(desc1);
+    _printLog(desc1);
     updateRequestState(desc1);
     var response = await http.get(url);
 
     var desc2 = "Response statusCode:${response.statusCode}";
-    print(desc2);
+    _printLog(desc2);
     updateRequestState("$desc1\n\n $desc2");
     var desc3 = "Response body:${response.body}";
-    print(desc3);
+    _printLog(desc3);
     updateRequestState("$desc1\n\n $desc2\n\n $desc3");
 
     //2. Create a client for multiple request usage. Don't forget to close the client when all task finished.
     var desc4 = "Request via http.Client() ...\n http.Client().get('$url')";
-    print(desc4);
+    _printLog(desc4);
     updateRequestState("$desc1\n\n$desc2\n\n$desc3\n\n$desc4\n\n");
     var client = http.Client();
     var response2 = await client.get(url);
     var desc5 = "Response statusCode: ${response2.statusCode}";
-    print(desc5);
+    _printLog(desc5);
     updateRequestState("$desc1\n\n$desc2\n\n$desc3\n\n$desc4\n\n$desc5\n\n");
     var desc6 = "Response body:${response2.body}";
-    print(desc6);
+    _printLog(desc6);
     updateRequestState(
         "$desc1\n\n$desc2\n\n$desc3\n\n$desc4\n\n$desc5\n\n$desc6\n\n");
     client.close();
@@ -209,19 +215,19 @@ class CommonNetworkState extends State<CommonNetwork> {
     var url = 'https://api.github.com';
     var httpClient = HttpClient();
     var desc1 = "Request via HttpClient...\nHttpClient().getUrl: $url";
-    print(desc1);
+    _printLog(desc1);
     updateRequestState(desc1);
     String result;
     try {
       var request = await httpClient.getUrl(Uri.parse(url));
       var response = await request.close();
       var desc2 = "Response statusCode:${response.statusCode}";
-      print(desc2);
+      _printLog(desc2);
       updateRequestState("$desc1\n\n$desc2");
       if (response.statusCode == HttpStatus.ok) {
         var json = await response.transform(utf8.decoder).join();
         var desc3 = "Response body:$json";
-        print(desc3);
+        _printLog(desc3);
         updateRequestState("$desc1\n\n $desc2\n\n $desc3");
         var data = jsonDecode(json);
         result = data['origin'];
